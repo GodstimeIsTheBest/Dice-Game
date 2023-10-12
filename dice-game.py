6b27d8d8669c6f8ef7ae1103a7ba7724820b2333
GodstimeIsTheBest <umunnakwegodstimee@gmail.com>
2023-10-12 13:39:35 +0100

    First commit

---
+++ dice-game.py
@@ -0,0 +1,7 @@
+pos=input("Enter type of POS:")
+MMO=input("Enter type of MMO:")
+Managing_director=input("Enter the name of md:")
+
+print("Good pos in the market is" + pos)
+print(MMO + " is licensed by the central bank of nigeria")
+print( Managing_director + " is the CEO TeasyPay")
